97943030313c834cfaeae72417e9f1ec75616578
liguoxiong <liguoxiong-it@bestpay.com.cn>
2017-09-18 17:40:32 +0800

    去掉多余的变量

--- src/rsa/RSASIGNTEST.java
+++ src/rsa/RSASIGNTEST.java
@@ -15,8 +15,6 @@ public class RSASIGNTEST {
 
     public static final String PRIVATE_KEY="MIIBUwIBADANBgkqhkiG9w0BAQEFAASCAT0wggE5AgEAAkEAh9DS6jvTog6w2ey6NciZ7d3PzCzfsq4C5yENbEhCQ5SGD5SB1sWWupkOChjMcnxjX9lfAZ5fk+8C3WWVW6GGIQIDAQABAkB4m9phhkV3WaJ1tILcdksz8FGzWHpC+8K6LCD2cujdh3H65ggkrF4ZfjTODDEMne2sVUW821/Hy0+/5pCoGMcdAiEA/U3LZ2rJdbTdgm9yyRiiEvkVoCJnwkyB5cK/hhKjt38CIQCJQuVr/zzp/+lp5m8IhlLhlXPjCZgl+ylApikv2saSXwIgHVjrDRNRPgLzew5AhU4GUR5sw/3Yeal1j1It8HGuaC8CIAaeSyGh9PXzePW6PrBSibyG0EeqNsPeEGclm+bKzbhRAiAl31t0VyyfDmYNIQwF2MREXaG7WtK1z4a3xnUxUxUv8g==";
 
-    public static final String CIPHER_ALGORITHM = "RSA";
-
     public static final String  SIGN_ALGORITHMS = "SHA1WithRSA";
 
     public static String sign(String content, String privateKey, String input_charset)
